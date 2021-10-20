d24548c766c31d75527ee749f4cbc5f77dd02554
Rvze <makarovzhest@mail.ru>
2021-10-20 03:52:24 +0300

    DAO

--- src/main/java/moonrock/clisocialnetwork/database/DAOs/MessagesDAO.java
+++ src/main/java/moonrock/clisocialnetwork/database/DAOs/MessagesDAO.java
@@ -1,8 +1,14 @@
 package moonrock.clisocialnetwork.database.DAOs;
 
+import moonrock.clisocialnetwork.entities.message.Message;
+import moonrock.clisocialnetwork.entities.user.User;
+
 /**
  * @author tsypk
  * @project CLISocialNetwork
  */
 public interface MessagesDAO {
+    Message getAll(User user1, User user2);
+
+    Message getLastMessage(User user1, User user2);
 }
--- src/main/java/moonrock/clisocialnetwork/database/DAOs/UsersDAOImpl.java
+++ src/main/java/moonrock/clisocialnetwork/database/DAOs/UsersDAOImpl.java
@@ -7,42 +7,101 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import java.util.ArrayList;
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * @author tsypk
  * @project CLISocialNetwork
  */
 public class UsersDAOImpl implements UsersDAO, HibernateConfigurer {
-    @Override
-    public User getUserByUsername(String username) {
-        return null;
-    }
 
     @Override
-    public ArrayList<User> getAll() {
+    public boolean isExist(String username, String password) {
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
             Transaction transaction = session.beginTransaction();
-
-
-            transaction.commit();
+            String sql = "SELECT username AND password FROM users WHERE" +
+                    "username =:username AND password = :password";
+            Query query = session.createNativeQuery(sql);
+            List<Object[]> list = query.setParameter("username", username).getResultList();
+            User user = (User) query.getSingleResult();
+            if (list.isEmpty()) {
+                transaction.commit();
+                session.close();
+                return false;
+            } else {
+                user.getPassword().equals(password);
+                transaction.commit();
+                session.close();
+                return true;
+            }
         }
-        return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            String sql = "SELECT * FROM users WHERE username = :username";
+
+            Query query = session.createNativeQuery(sql).addEntity(User.class);
+            query.setParameter("username", username);
+            User user = (User) query.getSingleResult();
+            transaction.commit();
+            session.close();
+            return user;
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            String sql = "SELECT * FROM users";
+            Query query = session.createNativeQuery(sql).addEntity(User.class);
+            List<User> userList = query.getResultList();
+            transaction.commit();
+            session.close();
+            return userList;
+        }
     }
 
     @Override
     public void addUser(User user) {
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            session.close();
 
+        }
     }
 
     @Override
     public void deleteUser(User user) {
-
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.remove(user);
+            transaction.commit();
+            session.close();
+        }
     }
 
     @Override
-    public void updateUserBio(User user, UserBio userBio) {
-
+    public void updateUserBio(String username, UserBio userBio) {
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, username);
+            user.setBio(userBio);
+            session.update(user);
+            transaction.commit();
+            session.close();
+        }
     }
 }
